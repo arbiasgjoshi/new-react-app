5eb0aecb056ed38e99e4064eb411c1a819881a39
Axel Archambeault <axel.archambeault@gmail.com>
2022-07-24 19:56:50 +1100

    replaced text for section tabs

--- src/components/organisms/sectionTabs/index.js
+++ src/components/organisms/sectionTabs/index.js
@@ -8,20 +8,28 @@ const SectionTabs = ({ activeSection }) => {
           <div className={styles.sectionWrapper}>
             <h1>Marvel</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Tincidunt ornare massa eget egestas. Egestas quis ipsum
-              suspendisse ultrices gravida dictum fusce ut. Viverra ipsum nunc
-              aliquet bibendum enim facilisis gravida neque. Egestas diam in
-              arcu cursus euismod quis viverra nibh. Cursus mattis molestie a
-              iaculis. Sed nisi lacus sed viverra tellus in hac. Quis lectus
-              nulla at volutpat diam ut venenatis tellus. Posuere sollicitudin
-              aliquam ultrices sagittis. Molestie nunc non blandit massa.
-              Ullamcorper malesuada proin libero nunc consequat. In ornare quam
-              viverra orci sagittis eu volutpat odio facilisis. Arcu non sodales
-              neque sodales ut etiam sit amet nisl. Ut ornare lectus sit amet.
-              Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec.
-              Bibendum neque egestas congue quisque egestas diam in arcu.
+              The Marvel Cinematic Universe (MCU) is an American media franchise
+              and shared universe centered on a series of superhero films
+              produced by Marvel Studios. The films are based on characters that
+              appear in American comic books published by Marvel Comics. The
+              franchise also includes television series, short films, digital
+              series, and literature. The shared universe, much like the
+              original Marvel Universe in comic books, was established by
+              crossing over common plot elements, settings, cast, and
+              characters.
+            </p>
+            <p>
+              The first MCU film, Iron Man (2008), began Phase One of the
+              series, which culminated in the 2012 crossover film The Avengers.
+              Phase Two began with Iron Man 3 (2013) and concluded with Ant-Man
+              (2015). Phase Three began with Captain America: Civil War (2016)
+              and concluded with Spider-Man: Far From Home (2019). The first
+              three phases in the franchise are collectively known as "The
+              Infinity Saga". The films of Phase Four began with Black Widow
+              (2021) and are set to conclude with Black Panther: Wakanda Forever
+              (2022). The fourth, fifth, and sixth phases of the MCU will
+              collectively make the second saga in the franchise, "The
+              Multiverse Saga".
             </p>
           </div>
         );
@@ -30,20 +38,32 @@ const SectionTabs = ({ activeSection }) => {
           <div className={styles.sectionWrapper}>
             <h1>DC</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Tincidunt ornare massa eget egestas. Egestas quis ipsum
-              suspendisse ultrices gravida dictum fusce ut. Viverra ipsum nunc
-              aliquet bibendum enim facilisis gravida neque. Egestas diam in
-              arcu cursus euismod quis viverra nibh. Cursus mattis molestie a
-              iaculis. Sed nisi lacus sed viverra tellus in hac. Quis lectus
-              nulla at volutpat diam ut venenatis tellus. Posuere sollicitudin
-              aliquam ultrices sagittis. Molestie nunc non blandit massa.
-              Ullamcorper malesuada proin libero nunc consequat. In ornare quam
-              viverra orci sagittis eu volutpat odio facilisis. Arcu non sodales
-              neque sodales ut etiam sit amet nisl. Ut ornare lectus sit amet.
-              Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec.
-              Bibendum neque egestas congue quisque egestas diam in arcu.
+              The DC Universe (DCU) is the fictional shared universe where most
+              stories in American comic book titles published by DC Comics take
+              place. DC superheroes such as Superman, Batman, Wonder Woman,
+              Martian Manhunter, The Flash, Green Lantern, Aquaman, and Green
+              Arrow are from this universe, as well as teams such as the Justice
+              League and the Teen Titans. It also contains well-known
+              supervillains such as Lex Luthor, The Joker, Sinestro, Harley
+              Quinn, Reverse-Flash, Darkseid, General Zod, The Penguin, the
+              Riddler, Deathstroke, Ra’s al Ghul, Bane, Two-Face, and The
+              Scarecrow. In context, the term "DC Universe" usually refers to
+              the main DC continuity.
+            </p>
+            <p>
+              The term "DC Multiverse" refers to the collection of all
+              continuities within DC Comics publications. Within the Multiverse,
+              the main DC Universe has gone by many names, but in recent years
+              has been referred to by "Prime Earth" (not to be confused with
+              "Earth Prime") or "Earth 0".
+            </p>
+            <p>
+              The main DC Universe, as well as the alternate realities related
+              to it, were quickly adapted to other media such as film serials or
+              radio dramas. In subsequent decades, the continuity between all of
+              these media became increasingly complex with certain storylines
+              and events designed to simplify or streamline the more confusing
+              aspects of characters' histories.
             </p>
           </div>
         );
